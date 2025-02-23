c62eac7fd9bf6a2d8b33fb9c77f8d5b7f9cb1ace
Parth Shrotri <90485296+parthshrotri@users.noreply.github.com>
2025-02-23 01:48:59 -0800

    Create body.py

---
+++ Dynamics/body.py
@@ -0,0 +1,17 @@
+class Body:
+    def __init__(self, name, planet_data, warn=False):
+        self.name   = name
+
+        self.radius         = float(planet_data["radius"])
+        self.mu             = float(planet_data["mu"])
+        self.J2             = float(planet_data["J2"])
+        self.colors         = planet_data["colors"]
+
+        if "atmosphere" in planet_data.keys():
+            planet_atmosphere   = planet_data["atmosphere"]
+            self.rho0           = float(planet_atmosphere["rho"])
+            self.scaleHeight    = float(planet_atmosphere["scaleHeight"])
+        else:
+            self.rho0           = 0
+            self.scaleHeight    = None
+            print(f"Warning: Atmosphere not supported for {self.name}")
